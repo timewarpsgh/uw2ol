63bbac61b27f83656b7b976ebc65b2affdacaa27
Lin <515441483@qq.com>
2020-06-30 17:48:56 +0800

    MenuClickHandlerForBattle -> handler for port

--- gui.py
+++ gui.py
@@ -92,8 +92,8 @@ class ButtonClickHandler():
 
     def on_button_click_items(self):
         dict = {
-            'Items': test,
-            'Discoveries': test,
+            'Items': self.menu_click_handler.items.on_menu_click_items,
+            'Discoveries': self.menu_click_handler.items.on_menu_click_discoveries,
             'Diary': test,
             'World Map': test,
             'Port Map': test
@@ -104,9 +104,9 @@ class ButtonClickHandler():
         dict = {
             'Set Target': test,
             'Enter Building': test,
-            'Enter Port': test,
+            'Enter Port': self.menu_click_handler.cmds.on_menu_click_enter_port,
             'Escort': test,
-            'Go Ashore': test,
+            'Go Ashore': self.menu_click_handler.cmds.on_menu_click_go_ashore,
             'View': test,
             'Gossip': test,
             'Battle': test,
@@ -145,9 +145,9 @@ class ButtonClickHandler():
 
     def on_button_click_battle(self):
 
-        if self.game.my_role.map == 'battle':
+        if 'battle' in self.game.my_role.map:
             dict = {
-                'View Enemy Ships': test,
+                'View Enemy Ships': self.menu_click_handler.battle.on_menu_click_view_enemy_ships,
                 'View My Ships': test,
                 'All Ships Move': test,
                 'Strategy': test,
@@ -159,16 +159,18 @@ class ButtonClickHandler():
 
 class MenuClickHandler():
     def __init__(self, game):
+        # need game info
         self.game = game
         self.ui_manager = game.ui_manager
 
+        # sub handlers
         self.ships = MenuClickHandlerForShips(game)
         self.mates = MenuClickHandlerForMates(game)
-        self.items = 3
-        self.cmds = 4
-        self.options = 5
-        self.port = 6  # has a few buildings
-        self.battle = 7
+        self.items = MenuClickHandlerForItems(game)
+        self.cmds = MenuClickHandlerForCmds(game)
+        self.options = MenuClickHandlerForOptions(game)
+        self.port = MenuClickHandlerForPort(game)  # has a few buildings
+        self.battle = MenuClickHandlerForBattle(game)
 
 class MenuClickHandlerForShips():
     def __init__(self, game):
@@ -255,3 +257,67 @@ class MenuClickHandlerForMates():
         # make window
         PanelWindow(pygame.Rect((59, 50), (350, 400)),
                     self.game.ui_manager, text, self.game)
+
+class MenuClickHandlerForItems():
+    def __init__(self, game):
+        self.game = game
+
+    def on_menu_click_items(self):
+        dict = {
+            f'Gold {self.game.my_role.gold}': test
+        }
+        self.game.button_click_handler.make_menu(dict)
+
+    def on_menu_click_discoveries(self):
+        my_discoveries = self.game.my_role.discoveries
+        dict = {}
+        for k in my_discoveries:
+            dict[str(k)] = test
+        self.game.button_click_handler.make_menu(dict)
+
+class MenuClickHandlerForCmds():
+    def __init__(self, game):
+        self.game = game
+
+    # def on_menu_click_set_target():
+    #     make_input_boxes('set_target', ['target name'])
+
+    def on_menu_click_enter_port(self):
+        self.game.change_and_send('change_map', ['port'])
+
+    def on_menu_click_go_ashore(self):
+        self.game.change_and_send('discover', [])
+
+    # def on_menu_click_battle():
+    #
+    #     try:
+    #         target_name = g_player.role.target_name
+    #         change_and_send('enter_battle_with', [target_name])
+    #     except:
+    #         print('no target!')
+
+class MenuClickHandlerForOptions():
+    def __init__(self, game):
+        self.game = game
+
+class MenuClickHandlerForPort():
+    def __init__(self, game):
+        self.game = game
+
+class MenuClickHandlerForBattle():
+    def __init__(self, game):
+        self.game = game
+
+    # def on_menu_click_all_ships_move():
+    #     change_and_send('all_ships_operate', [0])
+
+    def on_menu_click_view_enemy_ships(self):
+        # get enemy ships
+        enemy_ships = self.game.other_roles[self.game.my_role.enemy_name].ships
+
+        # new menu
+        dict = {}
+        for ship in enemy_ships:
+            dict[ship.name] = [self.game.button_click_handler.menu_click_handler.\
+                ships.on_menu_click_show_one_ship, [ship]]
+        self.game.button_click_handler.make_menu(dict)
