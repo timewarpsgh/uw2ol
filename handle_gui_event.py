b18a9d2ad6d8db2c9a44bfcbe09db20ecbe464f4
Lin <515441483@qq.com>
2020-07-02 13:40:18 +0800

    gui event refactored -> pygame event

--- handle_gui_event.py
+++ handle_gui_event.py
@@ -11,62 +11,68 @@ def handle_gui_event(self, event):
 
         # entry box entered
         if (event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED and
-                event.ui_object_id == '#main_text_entry'):
+                        event.ui_object_id == '#main_text_entry'):
+            entry_box_entered(self, event)
 
-            # unfocus
-            if event.text:
-                # send
-                self.change_and_send('speak', [event.text])
-
-                # clear
-                self.text_entry.set_text('')
-                self.text_entry.unfocus()
-                self.text_entry_active = False
-                print("entered:", event.text)
-
-        # button press
+        # button pressed
         elif event.user_type == pygame_gui.UI_BUTTON_PRESSED:
-
-            # normal buttons
-            if event.ui_element in self.buttons:
-                self.buttons[event.ui_element]()
-
-            # buttons in windows
-            elif event.ui_element in self.buttons_in_windows:
-
-                # get dict value
-                window = self.menu_stack[-1]
-                dict = window.dict
-                dict_value = dict['OK']
-
-                # if value is list
-                if isinstance(dict_value, list):
-                    protocol_name = dict_value[0]
-
-                    params_list = []
-                    for input_box in self.active_input_boxes:
-                        text = input_box.get_text()
-
-                        if text.isdigit():
-                            text = int(text)
-
-                        params_list.append(text)
-
-                    self.change_and_send(protocol_name, params_list)
-
-                # value is function
-                else:
-                    self.buttons_in_windows[event.ui_element]()
+            button_pressed(self, event)
 
         # selection list
         elif event.user_type == pygame_gui.UI_SELECTION_LIST_NEW_SELECTION:
-            if event.ui_element == self.selection_list_stack[-1]:
-                window = self.menu_stack[-1]
-                dict = window.dict
-                dict_value = dict[event.text]
+            selection_list_picked(self, event)
 
-                if isinstance(dict_value, list):
-                    function = dict[event.text][0]
-                    function(dict_value[1])
-                else:
-                    dict[event.text]()
+def entry_box_entered(self, event):
+    if event.text:
+        # send
+        self.change_and_send('speak', [event.text])
+
+        # clear
+        self.text_entry.set_text('')
+        self.text_entry.unfocus()
+        self.text_entry_active = False
+        print("entered:", event.text)
+
+def button_pressed(self, event):
+    # normal buttons
+    if event.ui_element in self.buttons:
+        self.buttons[event.ui_element]()
+
+    # buttons in windows
+    elif event.ui_element in self.buttons_in_windows:
+
+        # get dict value
+        window = self.menu_stack[-1]
+        dict = window.dict
+        dict_value = dict['OK']
+
+        # if value is list
+        if isinstance(dict_value, list):
+            protocol_name = dict_value[0]
+
+            params_list = []
+            for input_box in self.active_input_boxes:
+                text = input_box.get_text()
+
+                if text.isdigit():
+                    text = int(text)
+
+                params_list.append(text)
+
+            self.change_and_send(protocol_name, params_list)
+
+        # value is function
+        else:
+            self.buttons_in_windows[event.ui_element]()
+
+def selection_list_picked(self, event):
+    if event.ui_element == self.selection_list_stack[-1]:
+        window = self.menu_stack[-1]
+        dict = window.dict
+        dict_value = dict[event.text]
+
+        if isinstance(dict_value, list):
+            function = dict[event.text][0]
+            function(dict_value[1])
+        else:
+            dict[event.text]()
