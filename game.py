498b8a3008b218980c161afb40aef6799db6d979
Lin <515441483@qq.com>
2020-07-02 14:23:39 +0800

    handle pygame event refactored

--- game.py
+++ game.py
@@ -26,6 +26,7 @@ class Game():
         self.clock = pygame.time.Clock()
         pygame.display.set_caption(c.CAPTION)
         self.screen_surface = pygame.display.set_mode([c.WINDOW_WIDTH, c.WINDOW_HIGHT])
+        handle_pygame_event.init_key_mappings(self)
 
         # gui
         gui.init_gui(self)
--- handle_pygame_event.py
+++ handle_pygame_event.py
@@ -7,11 +7,9 @@ def handle_pygame_event(self, event):
     """argument self is game"""
     # quit
     if event.type == pygame.QUIT:
-        pygame.quit()
-        reactor.stop()
-        sys.exit()
+        quit(self, event)
 
-    # key
+    # key pressed
     elif event.type == pygame.KEYDOWN:
 
         # return (focus text entry)
@@ -21,75 +19,90 @@ def handle_pygame_event(self, event):
 
         # escape
         if event.key == pygame.K_ESCAPE:
-
-            # pop menu_stack
-            if len(self.menu_stack) > 0:
-                menu_to_kill = self.menu_stack.pop()
-                self.selection_list_stack.pop()
-                menu_to_kill.kill()
-            print(len(self.menu_stack))
-            print('escape pressed!')
-
-            # clear buttons_in_windows dict
-            self.buttons_in_windows.clear()
-            print('buttons_in_windows dict cleared!')
-
-            # deactivate text entry
-            self.text_entry_active = False
+            escape(self, event)
 
         # other keys
         if not self.text_entry_active:
-            if event.key == ord('p'):
-                print(self.my_role.name)
-            elif event.key == ord('1'):
-                self.connection.send('login', ['1', '1'])
-            elif event.key == ord('2'):
-                self.connection.send('login', ['2', '2'])
-            elif event.key == ord('3'):
-                self.connection.send('login', ['3', '3'])
-            elif event.key == ord('4'):
-                self.connection.send('login', ['4', '4'])
-            elif event.key == ord('5'):
-                self.connection.send('login', ['5', '5'])
-            elif event.key == ord('w'):
-                self.change_and_send('move', ['up'])
-            elif event.key == ord('s'):
-                self.change_and_send('move', ['down'])
-            elif event.key == ord('a'):
-                self.change_and_send('move', ['left'])
-            elif event.key == ord('d'):
-                self.change_and_send('move', ['right'])
-
-            elif event.key == ord('n'):
-                self.change_and_send('change_map', ['sea'])
-            elif event.key == ord('m'):
-                self.change_and_send('change_map', ['port'])
-
-            elif event.key == ord('b'):
-                self.change_and_send('try_to_fight_with', ['b'])
-            elif event.key == ord('e'):
-                self.change_and_send('exit_battle', [])
-            elif event.key == ord('v'):
-                self.change_and_send('try_to_fight_with', ['d'])
-
-            elif event.key == ord('k'):
-                self.change_and_send('shoot_ship', [0, 0])
+            other_keys(self, event)
 
     # user defined events
     elif event.type == EVENT_MOVE:
-        # print("got move event")
-
-        if self.my_role:
-            if self.move_direction == 1:
-                self.change_and_send('move', ['right'])
-            else:
-                self.change_and_send('move', ['left'])
-
-            self.move_count += 1
-
-            if self.move_count >= 15:
-                self.move_count = 0
-                self.move_direction *= -1
-
+        user_event_move(self, event)
     elif event.type == EVENT_HEART_BEAT:
         self.change_and_send('heart_beat', [])
+
+def quit(self, event):
+    pygame.quit()
+    reactor.stop()
+    sys.exit()
+
+def escape(self, event):
+    # pop menu_stack
+    if len(self.menu_stack) > 0:
+        menu_to_kill = self.menu_stack.pop()
+        self.selection_list_stack.pop()
+        menu_to_kill.kill()
+    print(len(self.menu_stack))
+    print('escape pressed!')
+
+    # clear buttons_in_windows dict
+    self.buttons_in_windows.clear()
+    print('buttons_in_windows dict cleared!')
+
+    # deactivate text entry
+    self.text_entry_active = False
+
+def init_key_mappings(self):
+    """cmds that change local state and sent to server"""
+    self.key_mappings = {
+        # move
+        'w': ['move', ['up']],
+        's': ['move', ['down']],
+        'a': ['move', ['left']],
+        'd': ['move', ['right']],
+
+        # change map
+        'n': ['change_map', ['sea']],
+        'm': ['change_map', ['port']],
+
+        # battle
+        'b': ['try_to_fight_with', ['b']],
+        'e': ['exit_battle', []],
+        'k': ['shoot_ship', [0, 0]],
+    }
+
+def other_keys(self, event):
+    # change and send keys
+    if chr(event.key) in self.key_mappings:
+        cmd = self.key_mappings[chr(event.key)][0]
+        params = self.key_mappings[chr(event.key)][1]
+        self.change_and_send(cmd, params)
+
+    # read keys
+    if event.key == ord('t'):
+        print(self.my_role.name)
+
+    # send keys
+    if event.key == ord('1'):
+        self.connection.send('login', ['1', '1'])
+    elif event.key == ord('2'):
+        self.connection.send('login', ['2', '2'])
+    elif event.key == ord('3'):
+        self.connection.send('login', ['3', '3'])
+    elif event.key == ord('4'):
+        self.connection.send('login', ['4', '4'])
+    elif event.key == ord('5'):
+        self.connection.send('login', ['5', '5'])
+
+def user_event_move(self, event):
+    if self.my_role:
+        if self.move_direction == 1:
+            self.change_and_send('move', ['right'])
+        else:
+            self.change_and_send('move', ['left'])
+
+        self.move_count += 1
+
+        if self.move_count >= 15:
+            self.move_count = 0
+            self.move_direction *= -1
