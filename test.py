b55abd7179776b4d141cf98154502388a8d2b200
Lin <515441483@qq.com>
2020-07-03 21:55:53 +0800

    hash_ports_meta_data added

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print(int(16/16))
+a = {
+	1: {
+	  'name': 'Lisbon',
+	  'x': 840,
+	  'y': 358,
+	  'economy': 780,
+	  'industry': 770,
+	  'allegiances': [
+		    100,
+		0,
+		0,
+		0,
+		0,
+		0,
+	  ],
+    }
+}
+
+print(a)
