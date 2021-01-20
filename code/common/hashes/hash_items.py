20bccc3e1b6780931ced80731f12d69cda423284
Lin <515441483@qq.com>
2021-01-20 11:15:31 +0800

    all items images added

--- code/common/hashes/hash_items.py
+++ code/common/hashes/hash_items.py
@@ -2,242 +2,297 @@ hash_items = {
 	###### Voyager's Aid #####
 		###### consumables #####
 		1:  {
-		  'name': 'Balm',
-		  'price': 1000, 
+            'name': 'Balm',
+            'price': 1000,
+            'image': [12, 7],
 		},
 		2:  {
-		  'name': 'Lime Juice',
-		  'price': 1000, 
+            'name': 'Lime Juice',
+            'price': 1000,
+            'image': [13, 7],
 		},	
 		3:  {
-		  'name': 'Rat Poison',
-		  'price': 500, 
+            'name': 'Rat Poison',
+            'price': 500,
+            'image': [11, 7],
 		},	
 	
 		###### tools #####
 		4:  {
-		  'name': 'Quadrant',
-		  'price': 4000, 
+            'name': 'Quadrant',
+            'price': 4000,
+            'image': [5, 7],
 		},	
 		5:  {
-		  'name': 'Sextant',
-		  'price': 8000, 
+            'name': 'Sextant',
+            'price': 8000,
+            'image': [6, 7],
 		},	
 		6:  {
-		  'name': 'Theodolite',
-		  'price': 12000, 
+            'name': 'Theodolite',
+            'price': 12000,
+            'image': [7, 7],
 		},		
 		7:  {
-		  'name': 'Pocket Watch',
-		  'price': 2000, 
+            'name': 'Pocket Watch',
+            'price': 2000,
+            'image': [8, 7],
 		},		
 		8:  {
-		  'name': 'Telescope',
-		  'price': 5000, 
+            'name': 'Telescope',
+            'price': 5000,
+            'image': [9, 7],
 		},	
 		9:  {
-		  'name': 'Cat',
-		  'price': 2000, 
+            'name': 'Cat',
+            'price': 2000,
+            'image': [10, 7],
 		},
 	
 		###### permits #####
 		10:  {
-		  'name': 'Tax Free Permit (E)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (E)',
+            'price': 10000,
+            'image': [7, 7],
+            'image': [15, 7],
 		},	
 		11:  {
-		  'name': 'Tax Free Permit (H)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (H)',
+            'price': 10000,
+            'image': [15, 7],
 		},	
 		12:  {
-		  'name': 'Tax Free Permit (I)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (I)',
+            'price': 10000,
+            'image': [15, 7],
 		},		
 		13:  {
-		  'name': 'Tax Free Permit (P)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (P)',
+            'price': 10000,
+            'image': [15, 7],
 		},		
 		14:  {
-		  'name': 'Tax Free Permit (S)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (S)',
+            'price': 10000,
+            'image': [15, 7],
 		},	
 		15:  {
-		  'name': 'Tax Free Permit (T)',
-		  'price': 10000, 
+            'name': 'Tax Free Permit (T)',
+            'price': 10000,
+            'image': [15, 7],
 		},		
 		
 	###### Treasure ######
 		###### Treasury ######
 		16:  {
             'name': 'Candleholder',
-		    'price': 3000,
+            'price': 3000,
             'image': [14,8],
 		},	
 		17:  {
-		  'name': 'Crown of Glory',
-		  'price': 50000, 
+            'name': 'Crown of Glory',
+            'price': 50000,
+            'image': [4, 8],
 		},	
 		18:  {
-		  'name': 'Garnet Brooch',
-		  'price': 20000, 
+            'name': 'Garnet Brooch',
+            'price': 20000,
+            'image': [15, 8],
 		},		
 		19:  {
-		    'name': 'Gold Bracelet',
-		    'price': 15000,
+            'name': 'Gold Bracelet',
+            'price': 15000,
             'image': [2,8],
 		},		
 		20:  {
-		  'name': 'Malachite Box',
-		  'price': 8000, 
-		},	
+            'name': 'Malachite Box',
+            'price': 8000,
+            'image': [5, 8],
+		},
 		21:  {
-		  'name': 'Mermaid Bangle',
-		  'price': 10000, 
+            'name': 'Mermaid Bangle',
+            'price': 10000,
+            'image': [2, 8],
 		},	
 		22:  {
-		  'name': 'Ruby Scepter',
-		  'price': 50000, 
+            'name': 'Ruby Scepter',
+            'price': 50000,
+            'image': [13, 8],
 		},	
 				
 		###### Accecory ######
 		23:  {
-		  'name': 'Aqua Tiara',
-		  'price': 5000, 
+            'name': 'Aqua Tiara',
+            'price': 5000,
+            'image': [12, 8],
 		},	
 		24:  {
-		  'name': 'China Dress',
-		  'price': 8000, 
-		},	
+            'name': 'China Dress',
+            'price': 8000,
+            'image': [7, 8],
+		},
 		25:  {
-		  'name': 'Circlet',
-		  'price': 4000, 
+            'name': 'Circlet',
+            'price': 4000,
+            'image': [9, 8],
 		},		
 		26:  {
-		  'name': 'Ermine Coat',
-		  'price': 12000, 
+            'name': 'Ermine Coat',
+            'price': 12000,
+            'image': [1, 8],
 		},		
 		27:  {
-		  'name': 'Peacock Fan',
-		  'price': 3000, 
-		},	
+            'name': 'Peacock Fan',
+            'price': 3000,
+            'image': [10, 8],
+		},
 		28:  {
-		  'name': 'Platinum Comb',
-		  'price': 10000, 
+            'name': 'Platinum Comb',
+            'price': 10000,
+            'image': [8, 8],
 		},	
 		29:  {
-		  'name': 'Silk Scarf',
-		  'price': 1000, 
+            'name': 'Silk Scarf',
+            'price': 1000,
+            'image': [6, 8],
 		},	
 		30:  {
-		  'name': 'Silk Shawl',
-		  'price': 3000, 
+            'name': 'Silk Shawl',
+            'price': 3000,
+            'image': [11, 8],
 		},	
 		31:  {
-		  'name': 'Velvet Coat',
-		  'price': 5000, 
-		},	
+            'name': 'Velvet Coat',
+            'price': 5000,
+            'image': [1, 8],
+		},
 		
 		
 	###### Armor ######	
 	32:  {
-	    'name': 'Chain Mail',
-	    'price': 2000,
+        'name': 'Chain Mail',
+        'price': 2000,
         'image': [2,7],
 	},	
 	33:  {
-	  'name': 'Leather Armor',
-	  'price': 1000, 
-	},	
+        'name': 'Leather Armor',
+        'price': 1000,
+        'image': [1, 7],
+	},
 	34:  {
-	  'name': 'Half Plate',
-	  'price': 4000, 
+        'name': 'Half Plate',
+        'price': 4000,
+        'image': [3, 7],
 	},		
 	35:  {
-	  'name': 'Plate Mail',
-	  'price': 8000, 
+        'name': 'Plate Mail',
+        'price': 8000,
+        'image': [4, 7],
 	},		
 	36:  {
-	  'name': 'Crusader Armor (*)',
-	  'price': 600000, 
+        'name': 'Crusader Armor (*)',
+        'price': 600000,
+        'image': [4, 7],
 	},	
 	37:  {
-	  'name': "Errol's Plate",
-	  'price': 300000, 
+        'name': "Errol's Plate",
+        'price': 300000,
+        'image': [4, 7],
 	},	
 		
 	###### Weapon ######
 		###### Curved Sword ######
 		38:  {
-		  'name': 'Short Saber',
-		  'price': 1000, 
-		},	
+            'name': 'Short Saber',
+            'price': 1000,
+            'image': [12, 6],
+		},
 		39:  {
-		  'name': 'Scimitar',
-		  'price': 8000, 
+            'name': 'Scimitar',
+            'price': 8000,
+            'image': [11, 6],
 		},	
 		40:  {
-		  'name': 'Japanese Sword',
-		  'price': 20000, 
+            'name': 'Japanese Sword',
+            'price': 20000,
+            'image': [13, 6],
 		},		
 		41:  {
-		  'name': 'Saber',
-		  'price': 3000, 
+            'name': 'Saber',
+            'price': 3000,
+            'image': [12, 6],
 		},		
 		42:  {
-		  'name': 'Magic Muramasa(*)',
-		  'price': 380000, 
+            'name': 'Magic Muramasa(*)',
+            'price': 380000,
+            'image': [8, 6],
 		},	
 		43:  {
-		  'name': "Siva's Sword(*)",
-		  'price': 280000, 
+            'name': "Siva's Sword(*)",
+            'price': 280000,
+            'image': [8, 6],
 		},			
 
 		###### Fencing Sword ######
 		44:  {
-		  'name': 'Flamberge',
-		  'price': 14000, 
+            'name': 'Flamberge',
+            'price': 14000,
+            'image': [15, 6],
 		},	
 		45:  {
-		  'name': 'Rapier',
-		  'price': 3000, 
+            'name': 'Rapier',
+            'price': 3000,
+            'image': [14, 6],
 		},	
 		46:  {
-		    'name': 'Epee',
-		    'price': 2000,
-            'image': [6,6],
+            'name': 'Epee',
+            'price': 2000,
+            'image': [14,6],
 		},		
 		47:  {
-		  'name': 'Estock',
-		  'price': 6000, 
+            'name': 'Estock',
+            'price': 6000,
+            'image': [9, 6],
 		},		
 		48:  {
-		  'name': 'Crusader Sword (*)',
-		  'price': 380000, 
+            'name': 'Crusader Sword (*)',
+            'price': 380000,
+            'image': [10, 6],
 		},		
 		
 		###### Heavy Sword ######
 		49:  {
-		  'name': 'Cutlass',
-		  'price': 1500, 
+            'name': 'Cutlass',
+            'price': 1500,
+            'image': [6, 6],
 		},	
 		50:  {
-		  'name': 'Broad Sword',
-		  'price': 5000, 
+            'name': 'Broad Sword',
+            'price': 5000,
+            'image': [7, 6],
 		},	
 		51:  {
-		  'name': 'Blue Crescent(*)',
-		  'price': 24000, 
+            'name': 'Blue Crescent(*)',
+            'price': 24000,
+            'image': [16, 6],
 		},		
 		52:  {
-		  'name': 'Claymore',
-		  'price': 15000, 
+            'name': 'Claymore',
+            'price': 15000,
+            'image': [15, 6],
 		},		
 		53:  {
-		  'name': 'Golden Dragon(*)',
-		  'price': 18000, 
+            'name': 'Golden Dragon(*)',
+            'price': 18000,
+            'image': [16, 6],
 		},			
-		
 
+		# treasure
+		54:  {
+            'name': 'Treasure Box',
+            'price': 100000,
+            'image': [16, 8],
+		},
 }
 
 
