758f7e1f8310d1818b0082d157bbe037f7c3ef0d
Lin <515441483@qq.com>
2021-02-24 20:26:15 +0800

    cannon alpha OK

---
+++ code/client/image_processor.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+
+def make_white_bg_transparent(img_file):
+    pic = Image.open(img_file)
+
+    pic = pic.convert('RGBA')  # 转为RGBA模式
+    width, height = pic.size
+    array = pic.load()  # 获取图片像素操作入口
+    for i in range(width):
+        for j in range(height):
+            pos = array[i, j]  # 获得某个像素点，格式为(R,G,B,A)元组
+            # 如果R G B三者都大于240(很接近白色了，数值可调整)
+            isEdit = (sum([1 for x in pos[0:3] if x > 240]) == 3)
+            if isEdit:
+                # 更改为透明
+                array[i, j] = (255, 255, 255, 0)
+
+    # 保存图片
+    pic.save('result.png')
+
+
+if __name__ == '__main__':
+    make_white_bg_transparent('cannon.png')
--- code/common/test.py
+++ code/common/test.py
@@ -1,5 +1,1 @@
-import random
 
-import itertools
-
-['Nao', 'Carrack', 'Galleon', 'Flemish Galleon', 'Venetian Galeass', 'Frigate', 'Barge', 'Full Rigged Ship']
