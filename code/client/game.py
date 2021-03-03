78d015ed2c57fb3150fd101051ac08999b2fa7a3
Lin <515441483@qq.com>
2021-03-03 20:17:18 +0800

    refactor game 1

--- code/client/game.py
+++ code/client/game.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 import constants as c
 from role import Role, Ship, Mate
 from map_maker import MapMaker
-from translator import Translator
 
 # code relocated to these files
 import draw
@@ -31,70 +30,64 @@ from sprites import Explosion
 
 class Game():
     def __init__(self):
-        # pygame
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        pygame.display.set_caption(c.CAPTION)
-        self.screen_surface = pygame.display.set_mode([c.WINDOW_WIDTH, c.WINDOW_HIGHT])
-        self.screen_surface_rect = self.screen_surface.get_rect()
-        self.movement = None
-        self.map_maker = MapMaker()
-        self.login_state_text = 'Please login or register.'
-        self.other_roles_rects = {}
-
-        # loop to change ship frame state
-        self.ship_frame = 1
-        looping_task = task.LoopingCall(self.change_ship_frame_state)
-        looping_task.start(0.5)
-
-        # translator
-        self.translator = Translator()
-
-        # gui
+        # pygame and gui
+        self._init_pygame()
         gui.init_gui(self)
 
-        # connection
-        self.connection = None
+        # loop to change ship frame state
+        self._init_ship_frame_loop()
 
-        # roles (data)
+        # roles (net data)
         self.my_role = None
         self.other_roles = {}
-        self.all_roles = {}
         Role.GAME = self
 
         # local data
-        self.think_time_in_battle = c.THINK_TIME_IN_BATTLE
-        self.time_of_day_index = 0
-        self.days_spent_at_sea = 0
-        self.time_of_day = 'day'
+        self._init_local_data()
 
         # load assets
-        self.font = None
-        self.images = {}
+        self._init_maps()
         self._load_assets()
-        self.building_text = ''
-
-        # NPCS
-            # static
-        self.dog = None
-        self.old_man = None
-        self.agent = None
-            # dynamic
-        self.man = None
-        self.woman = None
-
-        # port_map and sea_map only for grid change detection
-        self.port_map = PortMap()
-        self.sea_map = SeaMap()
 
         # sprite group
         self.all_sprites = pygame.sprite.Group()
         self.mark_sprites = pygame.sprite.Group()
 
-    def _load_assets(self):
+    def _init_pygame(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption(c.CAPTION)
+        self.screen_surface = pygame.display.set_mode(
+            [c.WINDOW_WIDTH, c.WINDOW_HIGHT])
+        self.screen_surface_rect = self.screen_surface.get_rect()
+
+    def _init_ship_frame_loop(self):
+        self.ship_frame = 1
+        looping_task = task.LoopingCall(self._change_ship_frame_state)
+        looping_task.start(0.5)
+
+    def _init_local_data(self):
+        self.think_time_in_battle = c.THINK_TIME_IN_BATTLE
+        self.time_of_day_index = 0
+        self.days_spent_at_sea = 0
+        self.time_of_day = 'day'
+        self.building_text = ''
+        self.login_state_text = 'Please login or register.'
+        self.other_roles_rects = {}
+
+    def _init_maps(self):
         # maps
+        self.map_maker = MapMaker()
         self.map_maker.set_world_map_tiles()
         self.map_maker.set_world_piddle()
+
+        # port_map and sea_map only for grid change detection
+        self.port_map = PortMap()
+        self.sea_map = SeaMap()
+
+    def _load_assets(self):
+        self.images = {}
+
         # load all imgs in these folders to self.images
         folder_names = ['buildings', 'huds', 'player', 'figures',
                         'discoveries_and_items', 'in_battle']
@@ -155,7 +148,7 @@ class Game():
             self.connection.send(protocol_name, params_list_in_str)
             return True
 
-    def change_ship_frame_state(self):
+    def _change_ship_frame_state(self):
         """invoded every 1 second for ship animation"""
         self.ship_frame *= -1
 
@@ -186,19 +179,22 @@ class Game():
 
                         now_grid_id = None
                         if my_role.map == 'sea':
-                            now_grid_id = self.sea_map.get_grid_id_by_x_and_y_tile_position(x_tile_pos, y_tile_pos)
+                            now_grid_id = self.sea_map.\
+                                get_grid_id_by_x_and_y_tile_position(x_tile_pos, y_tile_pos)
                         elif my_role.is_in_port():
-                            now_grid_id = self.port_map.get_grid_id_by_x_and_y_tile_position(x_tile_pos, y_tile_pos)
+                            now_grid_id = self.port_map.\
+                                get_grid_id_by_x_and_y_tile_position(x_tile_pos, y_tile_pos)
 
                         if now_grid_id != my_role.grid_id:
                             my_role.grid_id = now_grid_id
                             print(f"grid change to {now_grid_id}!!!!!!")
-                            self.connection.send('grid_change', [now_grid_id, my_role.direction])
+                            self.connection.send('grid_change',
+                                                 [now_grid_id, my_role.direction])
 
                         my_role.speed_counter = 0
 
                         # update sea image
-                        self.update_sea_image()
+                        self._update_sea_image()
 
             # other roles
             for role in self.other_roles.values():
@@ -220,7 +216,13 @@ class Game():
                             role.move([role.direction])
                             role.speed_counter = 0
 
-    def update_sea_image(self):
+    def _update_my_role_position(self):
+        pass
+
+    def _update_other_roles_positions(self):
+        pass
+
+    def _update_sea_image(self):
         if self.my_role.map == 'sea':
             my_tile_x = int(self.my_role.x / c.PIXELS_COVERED_EACH_MOVE)
             my_tile_y = int(self.my_role.y / c.PIXELS_COVERED_EACH_MOVE)
@@ -240,7 +242,8 @@ class Game():
                         time_of_day = c.TIME_OF_DAY_OPTIONS[self.time_of_day_index]
 
                         # make sea image
-                        self.images['sea'] = self.map_maker.make_partial_world_map(my_tile_x, my_tile_y, time_of_day)
+                        self.images['sea'] = self.map_maker.\
+                            make_partial_world_map(my_tile_x, my_tile_y, time_of_day)
 
 
 
