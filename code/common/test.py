c6a76b914ec9de8fa2c75ccd9d4f9c61f54173b3
Lin <515441483@qq.com>
2021-07-03 15:54:01 +0800

    1
    1

--- code/common/test.py
+++ code/common/test.py
@@ -1,2 +1,25 @@
-if 1 and 1 or 0:
-    print(1)
+from multiprocessing import Process
+import time
+
+
+def main():
+    while 1:
+        time.sleep(1)
+        print(1)
+
+
+
+class ClientProcess(Process):
+    def __init__(self):
+        Process.__init__(self)
+
+    def run(self):
+        main()
+
+
+if __name__ == "__main__":
+    # start clients
+    for i in range(2, 10):
+        p = ClientProcess()
+        p.start()
+        time.sleep(1)
